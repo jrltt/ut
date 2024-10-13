c2e08f0a722067e6ffc383e09e439e0348d69f44
Joaquin <hola@jrltt.net>
2024-10-13 12:12:30 +0200

    feat(login service): update path

--- src/services/login.service.ts
+++ src/services/login.service.ts
@@ -1,11 +1,9 @@
-const baseURL = "https://uptime.com/api/v1/";
-
 interface LoginProps {
   email: string;
   password: string;
 }
 export async function login({ email, password }: LoginProps) {
-  return fetch(`${baseURL}auth/login`, {
+  return fetch(`/api/auth/login`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
